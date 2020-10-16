c00f1dba238c2d8148648e02432d43d89edaf4fd
vincentbollaert <vincentbollaert@gmail.com>
2020-10-16 11:20:59 +0200

    added express-validator and basic validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
 
 const Router = express.Router()
 
-Router.post('/api/users/signup', (req, res) => {
-  res.send('awyeah!')
+Router.post('/api/users/signup', [
+  body('email').isEmail().withMessage('email must be valid'),
+  body('password').trim().isLength({ min: 5, max: 20 }).withMessage('password must be at least 5 characters')
+], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
+  const { email, password } = req.body
+  res.send(`creating user ${email}, ${password}`)
 })
 
 export { Router as signupRouter }
