bddbde897af2bbbe7259af187a1cc2e1031a744d
vincentbollaert <vincentbollaert@gmail.com>
2020-10-20 15:40:20 +0200

    add route-not-found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,6 +8,7 @@ import {
   signupRouter,
 } from './routes'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -15,6 +16,12 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+// capture error for all methods on undefined routes
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
 app.listen(3000, () => {
