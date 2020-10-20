415f28482d6fcf6c6a0ac8124a0a6655f7c71fbd
vincentbollaert <vincentbollaert@gmail.com>
2020-10-20 14:38:34 +0200

    format errors for consistency

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,12 +4,34 @@ import { RequestValidationError } from '../errors/request-validation-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
+    const errorsFormatted = {
+      errors: err.errors.map(x => ({
+        message: x.msg,
+        field: x.param,
+      }))
+    }
+
     console.log('validation error')
+    return res.status(400).send(errorsFormatted)
   }
   if (err instanceof DatabaseConnectionError) {
+    const errorsFormatted = {
+      errors: [
+        {
+          message: err.errorMessage
+        }
+      ]
+    }
     console.log('database error')
+    return res.status(500).send(errorsFormatted)
   }
-  res.status(400).send({
-    message: err.message
-  })
+
+  const errorFallbackFormatted = {
+    errors: [
+      {
+        message: 'something went wrong'
+      }
+    ]
+  }
+  res.status(400).send(errorFallbackFormatted)
 }
