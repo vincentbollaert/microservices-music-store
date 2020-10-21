0dbe4b907a8a787d48580b749c5902e3482ac4fa
vincentbollaert <vincentbollaert@gmail.com>
2020-10-21 20:43:13 +0200

    save user if validation passes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,36 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
 import { RequestValidationError } from '../errors/request-validation-error'
+import { UserModel } from '../models/users'
 
 const Router = express.Router()
 
 Router.post('/api/users/signup', [
-  body('email').isEmail().withMessage('email must be valid'),
-  body('password').trim().isLength({ min: 5, max: 20 }).withMessage('password must be at least 5 characters')
-], (req: Request, res: Response) => {
+  body('email')
+    .isEmail()
+    .withMessage('email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 5, max: 20 })
+    .withMessage('password must be at least 5 characters')
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array())
   }
 
-  throw new DatabaseConnectionError()
-
   const { email, password } = req.body
-  res.send(`creating user ${email}, ${password}`)
+  const existingUser = await UserModel.findOne({ email })
+
+  if (existingUser) {
+    console.log('email already in use')
+    return res.send({})
+  }
+
+  const user = UserModel.build({ email, password })
+  await user.save()
+  res.status(201).send(user)
 })
 
 export { Router as signupRouter }
