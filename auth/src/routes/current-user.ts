3937607e35525f33843f658d34c141d9d5905f24
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 22:40:55 +0200

    current-user endpoint returns & verify current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
 
 const Router = express.Router()
 
-Router.get('/api/users/currentuser', (req, res) => {
-  res.send('awyeah!')
+// the react app won't be able to inspect the cookie to get the current user, so hit this endpoint instead
+Router.get('/api/users/currentuser', (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+  
+  // if the jtw token has been messed with, verify will throw an error
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (error) {
+    res.send({ currentUser: null })
+  }
 })
 
 export { Router as currentUserRouter }
