3cc72e40def34a061b684b56c72e047b1e8aa562
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 23:21:50 +0200

    add require-auth middleware to current-user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express'
 import { verifyCurrentUser } from '../middlewares/verify-current-user'
+import { requireAuth } from '../middlewares/require-auth'
 
 const Router = express.Router()
 
 // the react app won't be able to inspect the cookie to get the current user, so hit this endpoint instead
-Router.get('/api/users/currentuser', verifyCurrentUser, (req: Request, res: Response) => {
+Router.get('/api/users/currentuser', verifyCurrentUser, requireAuth, (req: Request, res: Response) => {
   res.send({ currentUser: req.currentUser || null })
 })
 
