ab2708702e611155e019993dfa8848431cbcafe8
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 21:28:55 +0200

    validate correct signin email and pass, then cr8 JWT

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { UserModel } from '../models/users';
+import { Password } from '../services/password';
 
 const Router = express.Router()
 
@@ -14,9 +18,30 @@ Router.post('/api/users/signin', [
     .withMessage('must provide a password')
 ],
 validateRequest,
-(req: Request, res: Response) => {
+async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  const existingUser = await UserModel.findOne({ email })
 
-  res.send('awyeah!')
+  if (!existingUser) {
+    throw new BadRequestError('invalid credentials') // don't be too specific with this for hackzors
+  }
+
+  const isCorrectPassword = await Password.compare(existingUser.password, password)
+  if (!isCorrectPassword) {
+    throw new BadRequestError('invalid credentials')
+  }
+
+  // copy-pasted from signup
+  const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email,
+  }, process.env.JWT_KEY!)
+
+  req.session = {
+    jwt: userJwt
+  }
+
+  res.status(200).send(existingUser)
 })
 
 export { Router as signinRouter }
