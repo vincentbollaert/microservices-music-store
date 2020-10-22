9ec6123d7edcc6b80cdafe630f1cabf226faf65e
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 16:54:41 +0200

    added jsonwebtoken lib and saving to cokkie

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 import { BadRequestError } from '../errors/bad-request-error'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { UserModel } from '../models/users'
@@ -30,6 +31,19 @@ Router.post('/api/users/signup', [
 
   const user = UserModel.build({ email, password })
   await user.save()
+
+  // create jwt and save to cookie (session added to req with cookie-session lib)
+  // saving id and email for easy lookup once decrypted
+  // req.session possibly null so TS complains, instead of dot notation just set it as obj
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email,
+  }, 'someprivatekey')
+
+  req.session = {
+    jwt: userJwt
+  }
+
   res.status(201).send(user)
 })
 
