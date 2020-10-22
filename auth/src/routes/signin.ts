da000080c48d36a9240bba55b73966a794c75a2e
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 20:37:12 +0200

    extended signin route with basic validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,24 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body, validationResult } from 'express-validator'
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const Router = express.Router()
 
-Router.post('/api/users/signin', (req, res) => {
+Router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('must be a valid email'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('must provide a password')
+], (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array())
+  }
+
   res.send('awyeah!')
 })
 
