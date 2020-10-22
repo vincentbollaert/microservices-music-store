0e72506a92d17997ec22676ba5351b03ce066614
vincentbollaert <vincentbollaert@gmail.com>
2020-10-22 21:12:17 +0200

    minor code cleanup on error-handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,12 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
 import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof CustomError) {
     console.log('custom error')
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
-  const errorFallbackFormatted = [{ message: 'something went wrong' }]
-  res.status(400).send({ errors: errorFallbackFormatted })
+  res.status(400).send({ errors: [{ message: 'something went wrong' }] })
 }
