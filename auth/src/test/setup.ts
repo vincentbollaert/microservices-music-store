c4eb72170e068c25236bb6be8e84f3783ec290ce
vincentbollaert <vincentbollaert@gmail.com>
2020-10-23 14:30:07 +0200

    set JWT_KEY for tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 import { app } from '../app'
 
 let mongo: any
 beforeAll(async () => {
+  // was coming from k8s secret. need to set here as tests are not being run in cluster
+  process.env.JWT_KEY = 'somesthings'
+
   mongo = new MongoMemoryServer()
   const mongoUri = await mongo.getUri()
 
