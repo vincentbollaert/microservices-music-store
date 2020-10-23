a02d5e1b5734e5ba2c0065aff0ae5dc71952d01a
vincentbollaert <vincentbollaert@gmail.com>
2020-10-23 10:51:21 +0200

    start moving to mvc

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import { verifyCurrentUser } from '../middlewares/verify-current-user'
 import { requireAuth } from '../middlewares/require-auth'
+import { getCurrentUser } from '../controllers/currentuser'
 
 const Router = express.Router()
 
 // the react app won't be able to inspect the cookie to get the current user, so hit this endpoint instead
-Router.get('/api/users/currentuser', verifyCurrentUser, requireAuth, (req: Request, res: Response) => {
-  res.send({ currentUser: req.currentUser || null })
-})
+Router.get('/api/users/currentuser', [verifyCurrentUser, requireAuth], getCurrentUser)
 
 export { Router as currentUserRouter }
--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -1,4 +1,4 @@
-export { currentUserRouter } from './current-user'
+export { currentUserRouter } from './currentuser'
 export { signinRouter } from './signin'
 export { signoutRouter } from './signout'
 export { signupRouter } from './signup'
