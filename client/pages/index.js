dddb540578bc28547d08fe729f43bfd84dfcd5cc
vincentbollaert <vincentbollaert@gmail.com>
2020-10-28 16:16:09 +0200

    adjust endpoint based on context

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,27 @@
-const Home = () => <h1>homepage 33</h1>
+import axios from "axios"
 
+const Home = ({ data }) => {
+  console.log('data', data)
+  return (
+    <h1>homepage 33</h1>
+  )
+}
+Home.getInitialProps = async () => {
+  // getInitialProps also runs when navigating between pages, so need to know if this is exeuted by browser or server
+  // also, nginx looks at host to determine routing rules. for node need to set this manually as not part of url
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'microservices-music-store.dev'
+        }
+      }
+    ) 
+    return data
+  }
+
+  const { data } = await axios.get('/api/users/currentuser') 
+  return data
+}
 export default Home
