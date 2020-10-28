d65fb962cbbc1ad129e2194132f93cb214eaa51c
vincentbollaert <vincentbollaert@gmail.com>
2020-10-28 17:02:20 +0200

    added build-client fn to extract server/client logic

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import buildClient from "../api/build-client"
 
 const Home = (props) => {
   console.log('props', props)
@@ -10,19 +10,17 @@ Home.getInitialProps = async ({ req }) => {
   // getInitialProps also runs when navigating between pages, so need to know if this is exeuted by browser or server
   // also, nginx looks at host to determine routing rules. for node need to set this manually as not part of url
   // so need to set host header, but also cookie header (usually handled by browser), so just set all from req object
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    )
+  // if (typeof window === 'undefined') {
+  //   const { data } = await axios.get(
+  //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+  //     {
+  //       headers: req.headers
+  //     }
+  //   )
+  //   return data
+  // } else {
+    const { data } = await buildClient(req).get('api/users/currentuser') 
     return data
-  } else {
-    const { data } = await axios.get('/api/users/currentuser') 
-    return data
-  }
-
-  return {}
+  // }
 }
 export default Home
