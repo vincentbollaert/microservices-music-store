ac60eb5286e34e13264c2bd76f44cbdba8bd50d6
vincentbollaert <vincentbollaert@gmail.com>
2020-10-28 17:04:38 +0200

    moved comments to build-client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,10 +1,13 @@
 const axios = require("axios")
 
-const buildClient = (req) => {
+const buildClient = (context) => {
+  // getInitialProps also runs when navigating between pages, so need to know if this is exeuted by browser or server
+  // also, nginx looks at host to determine routing rules. for node need to set this manually as not part of url
+  // so need to set host header, but also cookie header (usually handled by browser), so just set all from req object
   if (typeof window === 'undefined') {
     return axios.create({
       baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/',
-      headers: req.headers
+      headers: context.req.headers
     })
   } else {
     return axios.create({
--- client/pages/index.js
+++ client/pages/index.js
@@ -6,21 +6,10 @@ const Home = (props) => {
     <h1>homepage 33</h1>
   )
 }
-Home.getInitialProps = async ({ req }) => {
-  // getInitialProps also runs when navigating between pages, so need to know if this is exeuted by browser or server
-  // also, nginx looks at host to determine routing rules. for node need to set this manually as not part of url
-  // so need to set host header, but also cookie header (usually handled by browser), so just set all from req object
-  // if (typeof window === 'undefined') {
-  //   const { data } = await axios.get(
-  //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-  //     {
-  //       headers: req.headers
-  //     }
-  //   )
-  //   return data
-  // } else {
-    const { data } = await buildClient(req).get('api/users/currentuser') 
-    return data
-  // }
+
+// apprently it is convention to refer to the entire async params as context
+Home.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('api/users/currentuser') 
+  return data
 }
 export default Home
