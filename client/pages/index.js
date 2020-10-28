7f6ab646489189daf3cbde2122d125e4115f5ef6
vincentbollaert <vincentbollaert@gmail.com>
2020-10-28 16:34:47 +0200

    instead of manually setting host, just set all headers

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,28 @@
 import axios from "axios"
 
-const Home = ({ data }) => {
-  console.log('data', data)
+const Home = (props) => {
+  console.log('props', props)
   return (
     <h1>homepage 33</h1>
   )
 }
-Home.getInitialProps = async () => {
+Home.getInitialProps = async ({ req }) => {
   // getInitialProps also runs when navigating between pages, so need to know if this is exeuted by browser or server
   // also, nginx looks at host to determine routing rules. for node need to set this manually as not part of url
+  // so need to set host header, but also cookie header (usually handled by browser), so just set all from req object
   if (typeof window === 'undefined') {
     const { data } = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'microservices-music-store.dev'
-        }
+        headers: req.headers
       }
-    ) 
+    )
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/currentuser') 
     return data
   }
 
-  const { data } = await axios.get('/api/users/currentuser') 
-  return data
+  return {}
 }
 export default Home
